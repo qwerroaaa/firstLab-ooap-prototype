ac0ee8495fd05d763848fb24e1d3e375131b217f
qwerroaaa <serbushko.qwe@gmail.com>
2024-03-12 14:34:57 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace OOAPwithPattern
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.drawBoard = new System.Windows.Forms.PictureBox();
+            this.drawRectangle = new System.Windows.Forms.Button();
+            this.drawCircle = new System.Windows.Forms.Button();
+            this.drawTriangle = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.drawBoard)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.drawTriangle);
+            this.panel1.Controls.Add(this.drawCircle);
+            this.panel1.Controls.Add(this.drawRectangle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(908, 91);
+            this.panel1.TabIndex = 0;
+            // 
+            // drawBoard
+            // 
+            this.drawBoard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.drawBoard.Location = new System.Drawing.Point(0, 91);
+            this.drawBoard.Name = "drawBoard";
+            this.drawBoard.Size = new System.Drawing.Size(908, 513);
+            this.drawBoard.TabIndex = 1;
+            this.drawBoard.TabStop = false;
+            this.drawBoard.MouseDown += new System.Windows.Forms.MouseEventHandler(this.drawBoard_MouseDown);
+            this.drawBoard.MouseUp += new System.Windows.Forms.MouseEventHandler(this.drawBoard_MouseUp);
+            // 
+            // drawRectangle
+            // 
+            this.drawRectangle.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.drawRectangle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.drawRectangle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.drawRectangle.Image = global::OOAPwithPattern.Properties.Resources.rectangle_icon;
+            this.drawRectangle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.drawRectangle.Location = new System.Drawing.Point(714, 23);
+            this.drawRectangle.Name = "drawRectangle";
+            this.drawRectangle.Size = new System.Drawing.Size(97, 47);
+            this.drawRectangle.TabIndex = 0;
+            this.drawRectangle.Text = "Прямоугольник";
+            this.drawRectangle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.drawRectangle.UseVisualStyleBackColor = false;
+            this.drawRectangle.Click += new System.EventHandler(this.drawRectangle_Click);
+            // 
+            // drawCircle
+            // 
+            this.drawCircle.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.drawCircle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.drawCircle.Image = global::OOAPwithPattern.Properties.Resources.circle_icon;
+            this.drawCircle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.drawCircle.Location = new System.Drawing.Point(611, 23);
+            this.drawCircle.Name = "drawCircle";
+            this.drawCircle.Size = new System.Drawing.Size(97, 47);
+            this.drawCircle.TabIndex = 1;
+            this.drawCircle.Text = "Круг";
+            this.drawCircle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.drawCircle.UseVisualStyleBackColor = false;
+            this.drawCircle.Click += new System.EventHandler(this.drawCircle_Click);
+            // 
+            // drawTriangle
+            // 
+            this.drawTriangle.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.drawTriangle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.drawTriangle.Image = global::OOAPwithPattern.Properties.Resources.triangle_icon;
+            this.drawTriangle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.drawTriangle.Location = new System.Drawing.Point(508, 23);
+            this.drawTriangle.Name = "drawTriangle";
+            this.drawTriangle.Size = new System.Drawing.Size(97, 47);
+            this.drawTriangle.TabIndex = 2;
+            this.drawTriangle.Text = "Треугольник";
+            this.drawTriangle.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.drawTriangle.UseVisualStyleBackColor = false;
+            this.drawTriangle.Click += new System.EventHandler(this.drawTriangle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 604);
+            this.Controls.Add(this.drawBoard);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.drawBoard)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button drawRectangle;
+        private System.Windows.Forms.PictureBox drawBoard;
+        private System.Windows.Forms.Button drawTriangle;
+        private System.Windows.Forms.Button drawCircle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOAPwithPattern
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private interface IShape
+        {
+            IShape Clone();
+            void Draw(Graphics graphics, Point startPoint, Point endPoint);
+        }
+
+        private class RectangleShape : IShape
+        {
+            public IShape Clone()
+            {
+                return new RectangleShape();
+            }
+
+            public void Draw(Graphics graphics, Point startPoint, Point endPoint)
+            {
+                Rectangle rect = GetRectangle(startPoint, endPoint);
+                graphics.DrawRectangle(new Pen(Color.Black, 2), rect);
+            }
+        }
+
+        private class TriangleShape: IShape
+        {
+            public IShape Clone()
+            {
+                return new TriangleShape();
+            }
+
+            public void Draw(Graphics graphics, Point startPoint, Point endPoint)
+            {
+                Point[] points = GetTriangle(startPoint, endPoint);
+                graphics.DrawPolygon(new Pen(Color.Black, 2), points);
+            }
+        }
+
+        private class CircleShape: IShape
+        {
+            public IShape Clone()
+            {
+                return new CircleShape();
+            }
+
+            public void Draw(Graphics graphics, Point startPoint, Point endPoint)
+            {
+                Rectangle rect = GetCircle(startPoint, endPoint);
+                graphics.DrawEllipse(new Pen(Color.Black, 2), rect);
+            }
+        }
+
+        private IShape rectanglePrototype = new RectangleShape();
+        private IShape trianglePrototype = new TriangleShape();
+        private IShape circlePrototype = new CircleShape();
+
+        private IShape selectedShape;
+        private Point startPoint;
+        private Point endPoint;
+
+        private void drawBoard_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e.Location;
+        }
+
+        private void drawBoard_MouseUp(object sender, MouseEventArgs e)
+        {
+            endPoint = e.Location;
+            DrawShape();
+        }
+
+        private void DrawShape()
+        {
+            if (selectedShape == null)
+            {
+                return;
+            }
+
+            Graphics graphics = drawBoard.CreateGraphics();
+            selectedShape.Draw(graphics, startPoint, endPoint);
+            graphics.Dispose();
+        }
+
+        private static Rectangle GetRectangle(Point startPoint, Point endPoint)
+        {
+            int width = Math.Abs(endPoint.X - startPoint.X); 
+            int height= Math.Abs(endPoint.Y - startPoint.Y);
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+
+            return new Rectangle(x, y, width, height);
+        }
+
+        private static Point[] GetTriangle(Point startPoint, Point endPoint)
+        {
+            Point[] points = new Point[3];
+            points[0] = startPoint;
+            points[1] = new Point(startPoint.X, endPoint.Y);
+            points[2] = endPoint;
+
+            return points;
+        }
+
+        private static Rectangle GetCircle(Point startPoint, Point endPoint)
+        {
+            int width = Math.Abs(endPoint.X - startPoint.X);
+            int height = Math.Abs(endPoint.Y - startPoint.Y);
+            int x = Math.Min(startPoint.X, endPoint.X);
+            int y = Math.Min(startPoint.Y, endPoint.Y);
+
+            return new Rectangle(x, y, width, height);
+        }
+
+        private void drawRectangle_Click(object sender, EventArgs e)
+        {
+            selectedShape= rectanglePrototype.Clone();
+        }
+
+        private void drawCircle_Click(object sender, EventArgs e)
+        {
+            selectedShape = circlePrototype.Clone();
+        }
+
+        private void drawTriangle_Click(object sender, EventArgs e)
+        {
+            selectedShape = trianglePrototype.Clone();
+        }
+    }
+}
